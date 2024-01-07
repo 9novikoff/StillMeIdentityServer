ae3300983e9936846744457f377871bf62a4417f
9novikoff <novikov.oleksandr10@gmail.com>
2024-01-07 21:08:20 +0200

    feat: add authorization services and middleware

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace StillMeIdentityServer
 {
@@ -8,7 +12,19 @@ namespace StillMeIdentityServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme);
+            builder.Services.AddAuthorizationBuilder();
+
+            builder.Services.AddDbContext<IdentityDbContext>(options => 
+                                                            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentityCore<User>()
+                            .AddEntityFrameworkStores<IdentityDbContext>()
+                            .AddApiEndpoints();
+
             var app = builder.Build();
+
+            app.MapIdentityApi<User>();
 
             app.Run();
         }
---
+++ User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StillMeIdentityServer
+{
+    public class User : IdentityUser
+    {
+        public byte[]? UserPicture { get; set; }
+    }
+}
